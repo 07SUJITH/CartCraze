288a6604bcb66497842cb70917ac35c3b5692b03
07SUJITH <tssujith2002@gmail.com>
2024-12-11 16:13:08 +0530

    feat: Implement routing and layout structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
-import { Button } from './components/ui/button';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => {
+import AboutUs from './app/aboutus/page';
+import CartPage from './app/cart/page';
+import CategoryGroup from './app/categories/page';
+import CategoryPage from './app/category/page';
+import CheckoutPage from './app/checkout/page';
+import Footer from './app/components/Footer';
+import Header from './app/components/Header';
+import MobileBottomNav from './app/components/MobileBottomNav';
+import WhatsAppButton from './app/components/WhatsAppButton';
+import { CartProvider } from './app/contexts/CartContext';
+import OfferPage from './app/offers/page';
+import HomePage from './app/page';
+import ProductPage from './app/product/page';
+import SearchResultsPage from './app/search/page';
+
+function App() {
   return (
-    <div className="h-screen w-screen text-2xl flex justify-center items-center">
-      <div>
-        <h4 className="text-4xl font-bold text-green-600">
-          Hello, Vite + React!
-        </h4>
-        <p className="text-xl">
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <Button>Button</Button>
+    <CartProvider>
+      <div className="flex min-h-screen flex-col bg-gray-50">
+        <Header />
+
+        <main className="flex-grow min-h-screen">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="*"
+              element={
+                <div className="container mx-auto px-4 py-6">
+                  <Routes>
+                    <Route path="/category/:slug" element={<CategoryPage />} />
+                    <Route path="/product/:id" element={<ProductPage />} />
+                    <Route path="/cart" element={<CartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/search" element={<SearchResultsPage />} />
+                    <Route path="/categories" element={<CategoryGroup />} />
+                    <Route path="/offers" element={<OfferPage />} />
+                    <Route path="/about" element={<AboutUs />} />
+                  </Routes>
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+
+        <Footer />
+        <WhatsAppButton />
+        <MobileBottomNav />
       </div>
-    </div>
+    </CartProvider>
   );
-};
+}
 
 export default App;
