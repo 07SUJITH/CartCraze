760ead08f749a1771b79e60f57331c68548cd3f1
07SUJITH <tssujith2002@gmail.com>
2024-12-11 16:12:56 +0530

    feat: Add Toaster component for improved user notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App.tsx';
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <App />
+      <Toaster />
     </Router>
   </StrictMode>,
 );
